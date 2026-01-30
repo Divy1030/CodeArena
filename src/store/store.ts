f20c20074b6df6b16ce41349517c53e4ba6c381c
vansh mac <vansh23164033@akgec.ac.in>
2026-01-30 14:57:41 +0530

    feat: Add persistor export to store for Redux persistence

--- src/store/store.ts
+++ src/store/store.ts
@@ -41,5 +41,7 @@ export const store = configureStore({
     }),
 });
 
+export const persistor = persistStore(store);
+
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
